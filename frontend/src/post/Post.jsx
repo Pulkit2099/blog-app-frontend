e7e5d588fa020547df70814ed023bf5f8d19b19f
Pulkit Methaniya <79719013+Pulkit2099@users.noreply.github.com>
2023-07-07 01:50:19 +0530

    Update Post.jsx

--- frontend/src/post/Post.jsx
+++ frontend/src/post/Post.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import "./post.css"
 
 const Post = ({post}) => {
-  const PF="http://localhost:5000/images/"
+  const PF="https://blog-app-t4i9.onrender.com/backend/images/"
   return (
     <div className="post">
     {post.photo&&<img className='postImg' src={PF+post.photo} alt=''/> }
